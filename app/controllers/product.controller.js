04b02a311dbb07aad2751596da3407a2d2acc16e
DevWithReact <atroxltd@gmail.com>
2022-05-25 18:47:08 +0900

    update product api

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -11,35 +11,107 @@ exports.buyProduct = async (req, res) => {
   if (!req.body.id || !req.body.address) {
     res.status(400).send({
       success: false,
-      message: "Content can not be empty!"
+      error: {
+        errorMessage: "Content can not be empty!"
+      }
     });
     return;
   }
 
-  const params = {
-    catalogId: req.body.id,
-    address: req.body.address,
-  };
-  
-  const catalog = await Catalog.findByPk(params.catalogId);
-  const user = await User.findByPk(params.address);
-  console.log(catalog, user);
-  res.send({})
-};
+  try {
+    const params = {
+      catalogId: req.body.id,
+      address: req.body.address,
+    };
+    
+    const catalog = await Catalog.findByPk(params.catalogId);
+    const user = await User.findByPk(params.address);
+    if (catalog == null) {
+      return res.status(400).send({
+        success: false,
+        error: {
+          errorMessage: "Can't find catalog by id"
+        }
+      })
+    }
+    if (user == null) {
+      return res.status(400).send({
+        success: false,
+        error: {
+          errorMessage: "Can't find user by id"
+        }
+      })
+    }
 
-// Retrieve all Catalogs from the database.
-exports.findAll = (req, res) => {
-  const name = req.query.name;
-  var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
+    if (user.cash1 <= catalog.cost1 || 
+      user.cash2 <= catalog.cost2 ||
+      user.cash3 <= catalog.cost3) {
+      return res.status(400).send({
+        success: false,
+        error: {
+          errorMessage: "User does not have enough cash"
+        }
+      })
+    }
 
-  Catalog.findAll({ where: condition })
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving catalogs."
-      });
+    const asset1 = await Asset.findOne({
+      where: {
+        type: 1,
+        address: params.address
+      }
     });
+    const asset2 = await Asset.findOne({
+      where: {
+        type: 2,
+        address: params.address
+      }
+    });
+    const asset3 = await Asset.findOne({
+      where: {
+        type: 3,
+        address: params.address
+      }
+    });
+
+    if (catalog.req1 != null && (asset1 == null || asset1.level > catalog.req1) ||
+        catalog.req2 != null && (asset2 == null || asset2.level > catalog.req2) ||
+        catalog.req3 != null && (asset3 == null || asset3.level > catalog.req3)
+      ) {
+      return res.status(400).send({
+        success: false,
+        error: {
+          errorMessage: "Asset level is higher than Catalog requirement."
+        }
+      })
+    }
+
+    //Passed the requirement
+    user.cash1 -= catalog.cost1;
+    user.cash2 -= catalog.cost2;
+    user.cash3 -= catalog.cost3;
+    await user.save();
+
+    // Save Product in the database
+    const product = {
+      address: params.address
+    };
+    await Product.create(product);
+    res.send({
+      success: true,
+      data: {
+        resources: {
+          cash1: user.cash1,
+          cash2: user.cash2,
+          cash3: user.cash3
+        }
+      }
+    })
+  } catch(err) {
+    res.status(500).send({
+      success: false,
+      error: {
+        errorMessage: err.message || "Some error occurred while buying product."
+      }
+    });
+  }
 };
